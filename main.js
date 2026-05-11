d0e97ab6183e020784c2c64e49e02406d91ed5a0
viniciusceolato <vinicius.ceolato@escola.pr.gov.br>
2026-05-11 08:53:51 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -14,35 +14,18 @@ for(let i=0;i <botoes.length;i++){
   }
   
 
-
-
-
-
-
-
-
-
-
 }const contadores = document.querySelectorAll(".contador");
 const tempoObjetivo1 = new Date ("2026-12-29T00:00:00");
-const tempoObjetivo2 = new Date ("2026-11-12T00:00:00");
-const tempoObjetivo3 = new Date ("2026-07-09T00:00:00");
-const tempoObjetivo4 = new Date ("2026-09-15T00:00:00");
+const tempoObjetivo2 = new Date ("2026-05-20T00:00:00");
+const tempoObjetivo3 = new Date ("2027-12-09T00:00:00");
+const tempoObjetivo4 = new Date ("2032-04-15T00:00:00");
 
-
-
-
-
-
-
-
-
+const tempos = [tempoObjetivo1,tempoObjetivo2,tempoObjetivo3,tempoObjetivo4]; 
 
 
 contadores[0].textContent = calculaTempo(tempoObjetivo1);
-contadores[1].textContent = calculaTempo(tempoObjetivo2);
-contadores[2].textContent = calculaTempo(tempoObjetivo3);
-contadores[3].textContent = calculaTempo(tempoObjetivo4);
+
+
 
 
 
@@ -53,12 +36,35 @@ function calculaTempo(tempoObjetivo){
   let minutos = Math.floor (segundos/60);
   let horas = Math.floor (minutos/60);
   let dias = Math.floor (horas/24);
-
+  
   segundos%= 60
   minutos%= 60
   horas%= 24
-
+  
+if (tempoFinal >0){
   return dias + " dias " + horas + " horas " + minutos + " minutos " + segundos + " segundos" ;
 
+} else {
+  return "prazo finalizado";
 }
+
+  
+}
+
+
+
+
+function atualizaCronometro(){
+  for (let i = 0; i < contadores.length; i++) {
+    contadores[i].textContent = calculaTempo(tempos[i]);
+  }
+
+}
+function comecaCronometro(){
+  atualizaCronometro();
+  setInterval(atualizaCronometro, 1000);
+  
+}
+
+comecaCronometro();
 
